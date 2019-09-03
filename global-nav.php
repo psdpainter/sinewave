06e42bf4b05078011a5ab41ba60de17ab65e1f87
psdpainter <6821005+psdpainter@users.noreply.github.com>
2019-09-03 15:46:49 -0500

    Update and rename global-nav.html to global-nav.php

---
+++ global-nav.php
@@ -0,0 +1,13 @@
+<nav class="global-nav">
+    <ul class="flush">
+        <li><a href="link-to-intranet">Intranet Home</a></li>
+        <li><a href="link-to-applications-directory">Applications Directory</a></li>
+        <!-- if you need to add a dropdown global menu item -->
+        <li class="is-dropdown">
+            <a href="javascript:void(0)">Dropdown Menu</a>
+            <ul class="dropdown">
+                <li><a href="javascript:void(0)">Dropdown Link</a></li>
+            </ul>
+        </li>
+    </ul>
+</nav>
