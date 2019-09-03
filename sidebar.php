8418fdc8b315f559fe08ab5925d0261bcdd7651e
psdpainter <6821005+psdpainter@users.noreply.github.com>
2019-09-03 15:45:11 -0500

    Update and rename sidebar.html to sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -1,14 +1,17 @@
 <aside class="sidebar">
     <div class="site-logo">
-        <img src="/assets/img/sinewave-logo.svg" class="w-100" />
+        <img src="/assets/img/dynetics-logo-blue.svg" class="w-100" />
     </div>
     <nav class="site-menu">
         <ul>
+            <!-- item without flyout(s) -->
+            <li>
+                <a href="javascript:void(0)">Sinewave</a>
+            </li>
+            <!-- item with flyout(s) -->
             <li class="is-flyout">
                 <a href="javascript:void(0)">Sinewave</a>
                 <ul class="flyout">
-                    <li><a href="/">Link</a></li>
-                    <li><a href="/">Link</a></li>
                     <li><a href="/">Link</a></li>
                 </ul>
             </li>
